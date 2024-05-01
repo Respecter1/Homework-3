3e146efb77978160a50f3ac6e98591ae98fc9b02
Евгений Рафальский <jerri081@icloud.com>
2024-05-01 20:05:21 +0300

    Add project files.

---
+++ homework#3.cpp
@@ -0,0 +1,25 @@
+/*
+	//Exircises 1
+
+a)Ваш вік : unsigned short кращій варіант для зберігання свого віку.
+в)Площа вашого заднього двору: float якщо треба більша точність то можна брати double або long double.
+с)Кількість зірок у галактиці: я оберу long double оскільки це дозволить зберігати дуже великі значення з більшою точністю
+d)Середній рівень випадання опадів за січень: як і з площею виберу float.
+*/
+
+int main()
+{
+	//Exircises 2
+	unsigned short myAge = 0;
+	float backyardArea = 0.0;
+	long double starInGalaxy = 0.0;
+	float janPrecip = 0.0;
+
+	//Exircises 3
+	const double PI = 3.14159;
+
+	//Exircises 4
+	float newPi = PI;
+
+	return 0;
+}
